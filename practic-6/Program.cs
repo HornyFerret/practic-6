bc8735e03b3df9ac53ea8cca6a1ed04f6aadd4fb
HornyFerret <ombro.de.morto@gmail.com>
2021-07-06 15:32:56 +0300

    Добавьте файлы проекта.

---
+++ practic-6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace practic_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число");
+            int gro = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваше число : {gro}");
+           
+            OneThing(gro);
+            TwoThing(gro);
+
+
+        }
+
+        public static void OneThing(int gro)
+        {
+            int zero = 0;
+            int fase = gro % 2;
+            if (fase == zero)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+            }
+            return;
+
+        }
+        public static void TwoThing(int gro)
+        {
+            int zeroone = 10;
+            if (gro>=zeroone)
+            {
+                Console.WriteLine("Число сложное");
+            }
+            else
+            {
+                Console.WriteLine("Число простое");
+            }
+            return;
+        }
+    }
+}
